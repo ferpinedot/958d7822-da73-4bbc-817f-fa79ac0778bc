6bd5157e660e8e61198d258eb8fad326f0241ef0
ferpinedot <mfernandapinedot@hotmail.com>
2019-10-01 17:21:38 -0500

    Monty Hall Fernanda Pinedo

--- monty-hall/main.py
+++ monty-hall/main.py
@@ -1,17 +1,14 @@
 import fire
-
 import pandas as pd
-
 from util import Game, Host, Guest, play_random_game
-
 
 class Main(object):
 
     @staticmethod
     def play_single_game(strategy="random", opts=3):
         # Get show-time elements
-        game = Game.random(number_of_options=opts)
-        host = Host(manage_game=game)
+        game = Game.random(number_of_options = opts)
+        host = Host(manage_game = game)
         guest = Guest()
         print(game)
 
@@ -22,6 +19,7 @@ class Main(object):
         # Host reveals another door
         revealed_door = host.reveal(given_guest_choice=first_choice)
         print("(t=2) Host reveals door with a goat: {}".format(revealed_door))
+
         # Final guest choice
         final_choice = guest.choose(options=host.show_options(), strategy=strategy)
         print("(t=3) Guest's final choice: {}".format(final_choice))
@@ -32,8 +30,29 @@ class Main(object):
 
     @staticmethod
     def play_multiple_games(strategy="random", times=100, opts=3, save=""):
-        # TODO: implement multiple games
-        raise NotImplementedError
+        result_ = []
+        for i in range(times):
+            game = play_random_game(number_of_options=opts, strategy=strategy)
+            result_.append(game)
+
+        def unique(list_):
+            unique_list = []
+            for x in list_:
+                if x not in unique_list:
+                    unique_list.append(x)
+            return unique_list
+
+        unique_ = unique(result_)
+        freq = []
+        for j in unique_:
+            freq.append(result_.count(j))
+
+        freq_prob = [x/times for x in freq]
+        result_df = pd.DataFrame(list(zip(freq, freq_prob)), index = unique_, columns = ['Frequency', 'Probability'])
+        print(result_df)
+        if save:
+            result_df.to_csv(save)
+        return
 
 
 if __name__ == "__main__":
